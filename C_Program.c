db60ef87e6eaddda893d72b4eba88a9eb9f3583b
dallen678 <dallen678@gmail.com>
2020-08-17 10:09:48 -0700

    I added a couple files

---
+++ C_Program.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+
+printf("hello! I'm begining a c program");
+
+return 0;
+
+}
---
+++ HelloJava.java
@@ -0,0 +1,10 @@
+class HelloJava{
+
+publilc static void main(String args[]){
+
+System.out.println("I'm begining Java!");
+
+	}
+
+
+}
